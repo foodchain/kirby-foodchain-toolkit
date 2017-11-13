35120b2c056e453cf7a17972e2725929d4e619ae
Paul Wagner <paulw@foodchain.com>
2017-11-13 15:51:20 -0800

    initializing

---
+++ foodchain-toolkit.php
@@ -0,0 +1,8 @@
+<?php
+// using the mini autoloader from /kirby/vendor/getkirby/toolkit/bootstrap.php
+load(array(
+  // classes
+  'fc_str' => __DIR__ . DS . 'lib' . DS . 'fc_str.php'
+ ));
+
+
---
+++ lib/fc_str.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * String
+ *
+ * A set of handy string methods
+ *
+ * @package   Kirby Toolkit
+ * @author    Bastian Allgeier <bastian@getkirby.com>
+ * @link      http://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+class FC_Str extends Str {
+
+  /**
+   * Creates a new yaml string from an array
+   *
+   * @param array $array
+   * @return string
+   */
+  public static function test($array) {
+    // don't include the opening dashes
+    return 'yo';
+  }
+
+
+
+}
