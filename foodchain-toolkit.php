7dc26add847db9aa9ee5190ea5d8bccfe5ad95eb
Paul Wagner <paulw@foodchain.com>
2017-11-20 15:37:53 -0800

    added removeIf method to cache class to allow expiring page caches programatically based on age

--- foodchain-toolkit.php
+++ foodchain-toolkit.php
@@ -4,7 +4,9 @@ load(array(
   // classes
   'fc\\str' => __DIR__ . DS . 'lib' . DS . 'fc_str.php',
   'fc\\f' => __DIR__ . DS . 'lib' . DS . 'fc_f.php',
-  'fc\\url' => __DIR__ . DS . 'lib' . DS . 'fc_url.php'
+  'fc\\url' => __DIR__ . DS . 'lib' . DS . 'fc_url.php',
+  'fc\\cache' => __DIR__ . DS . 'lib' . DS . 'fc_cache.php'
+
  ));
 
 
---
+++ lib/fc_cache.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace FC;
+
+use Cache as KirbyToolkitCache;
+
+/**
+ * File
+ *
+ * Low level file handling utilities to extend the core kirby methods
+ *
+ * @package   Kirby Foodchain Toolkit
+ * @author    Paul Wagner <paulw@foodchain.com>
+ * @link      https://github.com/foodchain
+ * @copyright Paul Wagner
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+class Cache extends KirbyToolkitCache {
+
+  /**
+   * remove a cache key if its older than a certian amount
+   * @param  Page    $page      [description]
+   * @param  integer $olderThan [description]
+   * @return [type]             [description]
+   */
+   public static function removeIf($page, $olderThan = 3600) {
+        //$cacheId is created the same way kirby does it
+        //in around line 603 in the kirby/kirby.php core file
+        $cacheId = md5(url::current() . $page->representation());
+        if (time() - cache::created($cacheId) > $olderThan) {
+          cache::remove($cacheId);
+        }
+  }
+
+}
+
